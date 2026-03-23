f7d8eda18df1a76b012dbf615a1be897b2008a35
sahil godwal <godwalsahil786@gmail.com>
2026-03-23 22:49:39 +0000

    cronjs added

--- src/server.js
+++ src/server.js
@@ -4,11 +4,14 @@ import dotenv, { parse } from 'dotenv';
 import {initDB} from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
+import job from './config/cron.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+
+if (process.enc.NODE_ENV === "production") job.start();
 
 app.use(rateLimiter);
 app.use(express.json()); //middleware to parse incoming JSON requests
@@ -19,6 +22,10 @@ app.use('/api/transactions', transactionsRoute)
 //     console.log("we hit a req method", req.method)
 //     next();
 // })
+
+app.get('/api/health', (req,res) => {
+    res.status(200).json({ status : "ok" });
+})
 
 
 
