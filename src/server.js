a627be3c99cf7f9e00ab3d5cba317685e58407f8
sahil godwal <godwalsahil786@gmail.com>
2026-05-25 21:12:44 +0100

    port change 3

--- src/server.js
+++ src/server.js
@@ -23,6 +23,10 @@ app.use('/api/transactions', transactionsRoute)
 //     next();
 // })
 
+app.get('/home', (req,res) => {
+    res.status(200).json({ status : "all ok" });
+})
+
 app.get('/api/health', (req,res) => {
     res.status(200).json({ status : "ok" });
 })
